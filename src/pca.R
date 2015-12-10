a4b4cf30344c630e89f33659a7eefc96ee3316a9
Kris Sankaran <sankaran.kris@gmail.com>
2015-12-10 12:07:59 -0800

    Plot all scores at once

--- src/pca.R
+++ src/pca.R
@@ -13,20 +13,17 @@ ggplot(tract_scores) +
   geom_text(aes(x = Comp.1, y = Comp.2, label = Tract2010), size = 3) +
   ggtitle("PCA Scores")
 
-## ---- pca-map-comp1 ----
+## ---- pca-map-comp ----
+tract_scores <- tract_scores %>%
+  melt() %>%
+  filter(variable %in% paste0("Comp.", 1:4))
 tract_scores_map <- merge(tract_scores, tract_map, by = "Tract2010")
-ggplot(tract_scores_map) +
-  geom_polygon(aes(x = long, y = lat, fill = Comp.1, group = group)) +
-  scale_fill_gradient2(midpoint = 0) +
-  coord_fixed() +
-  ggtitle("Shaded by comp.1")
 
-## ---- pca-map-comp2 ----
 ggplot(tract_scores_map) +
-  geom_polygon(aes(x = long, y = lat, fill = Comp.2, group = group)) +
+  geom_polygon(aes(x = long, y = lat, fill = value, group = group)) +
   scale_fill_gradient2(midpoint = 0) +
-  coord_fixed() +
-  ggtitle("Shaded by comp.2")
+  facet_wrap(~ variable) +
+  coord_fixed()
 
 ## ---- pca-loadings ----
 tract_loadings <- data.frame(variable = rownames(tract_pca$loadings), tract_pca$loadings[, 1:2])
