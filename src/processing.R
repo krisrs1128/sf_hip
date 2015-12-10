da0cc866f61b73da128d4e60313c6dbf61e11ba1
Kris Sankaran <sankaran.kris@gmail.com>
2015-12-10 12:09:03 -0800

    Center data, and split processing across files

---
+++ src/processing.R
@@ -0,0 +1,29 @@
+
+################################################################################
+# Preprocessing the SFHIP data for later analysis
+################################################################################
+
+## ---- preprocessing ----
+# filter tracts
+tract_info <- tract_info %>%
+  left_join(census_ids, by = "Tract2010")
+colnames(tract_info)[colnames(tract_info) == "Id"] <- "id"
+colnames(tract_info)[2:8] <- paste0("demo_", colnames(tract_info)[2:8])
+tract_info <- tract_info[tract_info$demo_Pop2010 > 1000, ]
+
+# get map data
+gpclibPermit()
+tract <- readOGR(dsn = "../data/processed_data/gz_2010_06_140_00_500k", layer = "gz_2010_06_140_00_500k")
+
+tract <- fortify(tract, region = "GEO_ID")
+tract_map <- merge(tract_info, tract, by = "id", all.x = TRUE)
+tract_num <- tract_info %>%
+  select(starts_with("demo"), starts_with("crime"), starts_with("license")) %>%
+  as.matrix()
+
+# some filtering and transformations
+tract_num <- tract_num[, colSums(tract_num != 0) >= 30]
+tract_num <- (tract_num ^ (1 / 3) - 1) / (1 / 3)
+tract_num <- scale(tract_num, center = T, scale = F)
+
+rownames(tract_num) <- tract_info$Tract2010
--- src/summaries.R
+++ src/summaries.R
@@ -3,30 +3,6 @@
 # Exploratory summaries of the SF HIP data
 ################################################################################
 
-## ---- preprocessing ----
-# filter tracts
-tract_info <- tract_info %>%
-  left_join(census_ids, by = "Tract2010")
-colnames(tract_info)[colnames(tract_info) == "Id"] <- "id"
-colnames(tract_info)[2:8] <- paste0("demo_", colnames(tract_info)[2:8])
-tract_info <- tract_info[tract_info$demo_Pop2010 > 1000, ]
-
-# get map data
-gpclibPermit()
-tract <- readOGR(dsn = "../data/processed_data/gz_2010_06_140_00_500k", layer = "gz_2010_06_140_00_500k")
-
-tract <- fortify(tract, region = "GEO_ID")
-tract_map <- merge(tract_info, tract, by = "id", all.x = TRUE)
-tract_num <- tract_info %>%
-  select(starts_with("demo"), starts_with("crime"), starts_with("license")) %>%
-  as.matrix()
-
-# filter questions, and cube root
-tract_num <- tract_num[, colSums(tract_num != 0) >= 30]
-tract_num <- (tract_num ^ (1 / 3) - 1) / (1 / 3)
-
-rownames(tract_num) <- tract_info$Tract2010
-
 ## ---- map-demo ----
 ggplot(tract_map) +
   geom_polygon(aes(x = long, y = lat, group = group, fill = demo_Pop2010)) +
